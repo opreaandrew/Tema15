f13688ac07cd44d92f3dfca641f813be6dd44e50
Andrei Oprea <opreaandrew@yahoo.com>
2023-10-25 05:53:13 +0300

    Barely did anything

---
+++ src/main/java/ro/fasttrackit/TemperatureConversion.java
@@ -0,0 +1,12 @@
+package ro.fasttrackit;
+
+public class TemperatureConversion {
+
+    public double celsiusToFahrenheit(double celsius){
+
+    }
+
+    public double fahrenheitToCelsius(double fahrenheit){
+
+    }
+}
---
+++ src/test/java/ro/fasttrackit/TemperatureConversionTest.java
@@ -0,0 +1,16 @@
+package ro.fasttrackit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConversionTest {
+
+    @Test
+    void celsiusToFahrenheit() {
+    }
+
+    @Test
+    void fahrenheitToCelsius() {
+    }
+}
